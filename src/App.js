062400b2ad725a526b2c044717d72fb4da5c46e2
Arjun Patidar <61613329+arjunpatidarofficial@users.noreply.github.com>
2021-07-24 00:47:34 +0530

    Tailwindcss configuration and react routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/login" exact component={Login} />
+				<Route path="/signup" exact component={Register} />
+			</Switch>
+		</>
+	);
 }
 
 export default App;
